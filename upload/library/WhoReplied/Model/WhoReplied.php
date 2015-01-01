d187939f79e4cdeb5bcbeda94de5cd1cf32c32cd
Xon <tazipper1@hotmail.com>
2015-01-01 18:39:07 +0800

    Use existing xf_thread_user_post table to save querying a long thread for posts by users. Should solve the performance impact of this addon on long threads.

--- upload/library/WhoReplied/Model/WhoReplied.php
+++ upload/library/WhoReplied/Model/WhoReplied.php
@@ -9,18 +9,19 @@ class WhoReplied_Model_WhoReplied extends XenForo_Model
 	 */
 	public function getUserAndCountPosts($thread)
 	{
-		return $this->fetchAllKeyed("
-						SELECT COUNT(posts.post_id) as post_count, user.*
-							FROM xf_post as posts
-						INNER JOIN xf_user AS user
-							ON user.user_id = posts.user_id
-						WHERE posts.thread_id = '". $thread['thread_id'] . "' AND
-							  posts.position <> 0 AND
-							  posts.message_state ='visible'
-						GROUP BY user.user_id
-						ORDER BY post_count DESC
-						"
-				, "user_id");
+        $users = $this->fetchAllKeyed("
+            SELECT post_count, user.*
+            FROM xf_thread_user_post posts
+            JOIN xf_user user ON user.user_id = posts.user_id
+            WHERE posts.thread_id = '". $thread['thread_id'] . "'
+            ORDER BY post_count DESC
+            ", "user_id");
+        // remove the first post from the post count
+        if (isset($users[$thread['user_id']]))
+        {
+            $users[$thread['user_id']]['post_count'] = $users[$thread['user_id']]['post_count'] - 1;
+        }
+		return $users;
 	}
 
 
