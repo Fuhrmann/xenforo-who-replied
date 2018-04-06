c7fa584821f76fae4412e5ebec1b58cc9ce2a974
Xon <tazipper1@hotmail.com>
2018-04-07 05:00:30 +0800

    Installer now requires php 5.4+ and XF1.3+

--- upload/library/WhoReplied/Installer.php
+++ upload/library/WhoReplied/Installer.php
@@ -2,10 +2,22 @@
 
 class WhoReplied_Installer
 {
-    public static function install($installedAddon, array $addonData, SimpleXMLElement $xml)
+    public static function install($existingAddOn, array $addonData, SimpleXMLElement $xml)
     {
-        $version = isset($installedAddon['version_id']) ? $installedAddon['version_id'] : 0;
-
+        $version = isset($existingAddOn['version_id']) ? $existingAddOn['version_id'] : 0;
+        $required = '5.4.0';
+        $phpversion = phpversion();
+        if (version_compare($phpversion, $required, '<'))
+        {
+            throw new XenForo_Exception(
+                "PHP {$required} or newer is required. {$phpversion} does not meet this requirement. Please ask your host to upgrade PHP",
+                true
+            );
+        }
+        if (XenForo_Application::$versionId < 1030070)
+        {
+            throw new XenForo_Exception('XenForo 1.3.0+ is Required!', true);
+        }
         $db = XenForo_Application::getDb();
 
         if ($version == 0)
